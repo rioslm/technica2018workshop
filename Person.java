487f3e3181d2991581d15f0581c4c71a94cc878c
mvpea <pearceav@dukes.jmu.edu>
2018-11-10 17:39:36 -0500

    adding the Person object

---
+++ Person.java
@@ -0,0 +1,14 @@
+/**
+ * A person object.
+ *
+ * @author Mandy Pearce
+  *@version 11.10.18
+ */
+public class Person()
+{
+	public static int teamId;
+	public static String school;
+	public static String name;
+	public static String email;
+	public static String phoneNum;
+}
