ef583d68d73631e8b9d8b9b0d4587e773bfbc30e
mvpea <pearceav@dukes.jmu.edu>
2018-11-10 17:58:12 -0500

    done with person constructor

--- Person.java
+++ Person.java
@@ -6,9 +6,25 @@
  */
 public class Person()
 {
-	public static int teamId;
-	public static String school;
 	public static String name;
 	public static String email;
 	public static String phoneNum;
+	public static String school;
+	public static int teamId;
+
+	/**
+	 * Person constructor initializes the fields of the person object.
+	 * @param name the name of the person.
+	 * @param email the email of the person.
+	 * @param phoneNum the phone number of the person.
+	 * @param school of the person.
+	 * @param teamID of person, if no team set to 0
+	*/
+	public Person(String name, String email, String phoneNum, String school, int teamID){
+		this.name = name;
+		this.email = email;
+		this.phoneNum = phoneNum;
+		this.school = school;
+		this.teamID = teamID;
+	}
 }
